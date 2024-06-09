f5e91ec3476b7a708872bc80f9e07c2b58eca425
bertsilvaa20 <robertsilvaqueiroz22@gmail.com>
2024-06-09 15:57:53 -0300

    resumo de tipos e variaveis

---
+++ java-tipos-e-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,32 @@
+public class TiposVariaveis{
+
+public static void main(String[] args) {
+
+    String meuNome = "Robert Silva";
+
+    double salarioMinimo = 2500; // double salarioMinimo = 2.500 *XXXX* FORMA INCORRETA
+
+    short numeroCurto = 1;
+    int numeroNormal = numeroCurto;
+    short numeroCurto2 = (short) numeroNormal;
+
+    final int NUMERO = 1;
+
+    NUMERO = 3;
+
+    final double VALOR_DE_PI = 3.14;
+
+    VALOR_DE_PI = 3.15;
+
+    System.out.println(NUMERO);
+    System.out.println(VALOR_DE_PI);
+    
+
+
+
+
+}
+
+
+
+}
