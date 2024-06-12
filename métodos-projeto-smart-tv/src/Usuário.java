74b389ad34d11aac2489bf8a9fe63c84e1598360
bertsilvaa20 <robertsilvaqueiroz22@gmail.com>
2024-06-12 14:23:00 -0300

    projeto da SmartTV - prática Métodos

---
+++ métodos-projeto-smart-tv/src/Usuário.java
@@ -0,0 +1,40 @@
+public class Usuário {
+    
+    public static void main(String [] args){
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV está ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status -> TV ligada? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> TV ligada? " + smartTv.ligada);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("Novo Status -> Volume atual: " + smartTv.volume);
+
+        smartTv.mudarCanal(13);
+
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.diminuirCanal();
+        smartTv.diminuirCanal();
+        
+        smartTv.mudarCanal(33);
+
+
+
+
+
+    }
+    
+}
