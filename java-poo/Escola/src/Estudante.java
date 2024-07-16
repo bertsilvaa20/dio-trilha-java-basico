5ebf05617e45898fd192dd10056fc13ffcfd72a4
bertsilvaa20 <robertsilvaqueiroz22@gmail.com>
2024-07-16 20:54:34 -0300

    exemplo de pacotes

--- java-poo/Escola/src/Estudante.java
+++ java-poo/Escola/src/Estudante.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 public class Estudante {
 	
 	String nome;
@@ -8,14 +6,3 @@ public class Estudante {
 	char sexo;
 
 }
-=======
-
-public class Estudante {
-	
-	String nome;
-	int idade;
-	String raça;
-	char sexo;
-
-}
->>>>>>> 13524309c936f019ea7e80c63ab13c196fa003a3
